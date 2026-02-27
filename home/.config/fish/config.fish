fb37f027197b12e0ffd74128e5024e9666080f31
Yorick van der Vis <pf57ma@insim.biz>
2026-02-27 11:18:59 +0100

    auto-start zellij on fish startup

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,6 +1,11 @@
 # Disable greeting
 set fish_greeting
 
+# Auto-start zellij (skip if already inside a session)
+if status is-interactive && test -z "$ZELLIJ"
+    zellij
+end
+
 # Editor
 set -gx EDITOR nvim
 set -gx MANPAGER 'nvim +Man!'
