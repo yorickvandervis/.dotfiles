4abe46d6f4f43c6ea8485f58478a377b2ea0fe96
Yorick van der Vis <pf57ma@insim.biz>
2026-06-25 09:05:41 +0200

    fish: default prompt, disable zellij autostart

--- home/.config/fish/conf.d/bun.fish
+++
@@ -1,2 +0,0 @@
-set -gx BUN_INSTALL $HOME/.bun
-fish_add_path $BUN_INSTALL/bin
--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,11 +1,6 @@
 # Disable greeting
 set fish_greeting
 
-# Auto-start zellij (skip if already inside a session)
-if status is-interactive && test -z "$ZELLIJ"
-    zellij
-end
-
 # Editor
 set -gx EDITOR nvim
 set -gx MANPAGER 'nvim +Man!'
